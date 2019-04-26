e88500beedb22ef641cb75bd1a6cfbb688ba8e7c
Vita <vita.petrovskaya@gmail.com>
2019-04-26 20:06:04 +0300

    Add front-end components

---
+++ CertificationCenter/CertificationCenter/ClientApp/src/app/course/course-delete/course-delete.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-delete-component',
+  templateUrl: './course-delete.component.html'
+})
+export class CourseDeleteComponent {
+  public id = 0;
+
+  public deleteCourse() {
+
+  }
+}
