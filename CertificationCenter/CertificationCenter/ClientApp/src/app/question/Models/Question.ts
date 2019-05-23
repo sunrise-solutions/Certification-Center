08429275cb5b9efc6b41789c0613dd65c39b1a0c
Vita <vita.petrovskaya@gmail.com>
2019-05-23 18:08:51 +0300

    Question (not quite working)

---
+++ CertificationCenter/CertificationCenter/ClientApp/src/app/question/Models/Question.ts
@@ -0,0 +1,16 @@
+export class Question{
+    id: number;
+    description: string;
+    answer1: string;
+    IsAnswer1True: boolean;
+    answer2: string;
+    IsAnswer2True: boolean;
+    answer3: string;
+    IsAnswer3True: boolean;
+    answer4: string;
+    IsAnswer4True: boolean;
+    answer5: string;
+    IsAnswer5True: boolean;
+    topicId: number;
+    courseId: number;
+}
